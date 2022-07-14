b3ad07f089dda0908329c30cbff7b19a267f62de
Eric Hu <eric-hu@users.noreply.github.com>
2022-07-14 15:26:39 -0700

    Use rack-request-id

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ class App < Sinatra::Base
     Thread.current[:calls] +=1
 
     puts "Current number of calls received: #{Thread.current[:calls]}"
+    puts "Current request_id: #{Thread.current[:request_id]}"
     puts "-----"
 
     'Hello world!'
--- config.ru
+++ config.ru
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require_relative './app.rb'
+require 'rack-request-id'
 
 map '/' do
+  use Rack::RequestId
   run App
 end
